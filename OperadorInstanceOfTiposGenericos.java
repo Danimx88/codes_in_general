7789437df3d4197c3fcba44b4c023c8a3e40029f
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-17 03:27:37 -0500

    Create OperadorInstanceOfTiposGenericos.java

---
+++ OperadorInstanceOfTiposGenericos.java
@@ -0,0 +1,56 @@
+public class OperadorInstanceOfTiposGenericos {
+    public static void main(String[] args) {
+        String texto = "Creando un objeto de la clase String....que tal!";
+        Object texto1 = "Creando un objeto de la clase String....que tal!";
+
+        Number num = Integer.valueOf(7); //7;
+
+        Boolean b1 = texto instanceof  String;
+
+        System.out.println("texto es del tipo String = " + b1);
+
+        b1 = texto instanceof  Object;
+        System.out.println("texto es del tipo Object = " + b1);
+
+        b1 = texto1 instanceof  Integer;
+        System.out.println("texto1 es del tipo Integer = " + b1);
+
+        b1 = num instanceof  Integer;
+        System.out.println("texto es del tipo Integer = " + b1);
+
+        b1 = num instanceof  Number;
+        System.out.println("num es del tipo Number = " + b1);
+
+        b1 = num instanceof  Object;
+        System.out.println("num es del tipo Object = " + b1);
+
+        b1 = num instanceof  Long;
+        System.out.println("num es del tipo Long = " + b1);
+
+        b1 = num instanceof  Double;
+        System.out.println("num es del tipo Double = " + b1);
+
+        Number decimal = Double.valueOf(45.54); //todos los numeros son number (tipo generico)
+        b1 = decimal instanceof Double;
+        System.out.println("decimal es del tipo Double = " + b1);
+
+        b1 = decimal instanceof Float;
+        System.out.println("decimal es del tipo Float = " + b1);
+
+        b1 = decimal instanceof  Number;
+        System.out.println("decimal es del tipo Number = " + b1);
+
+        b1 = b1 instanceof Boolean;
+        System.out.println("b1 es del tipo Boolean = " + b1);
+
+        Number decimal0 = Float.valueOf(45.54f); //Float tipo generico
+        b1 = decimal0 instanceof Double;
+        System.out.println("decimal0 es del tipo Double = " + b1);
+
+        b1 = decimal0 instanceof Float;
+        System.out.println("decimal0 es del tipo Float = " + b1);
+
+        b1 = decimal0 instanceof Integer;
+        System.out.println("decimal0 es del tipo Integer = " + b1);
+    }
+}
