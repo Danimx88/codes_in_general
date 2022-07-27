2293d1af5b28ae86276425858501742dcbf96444
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-27 13:25:53 -0500

    Create Figure.java

---
+++ Figure.java
@@ -0,0 +1,21 @@
+/*
+*
+*@authhor Daniel
+*
+*/
+public class Figure {
+
+    public static void main(String[] args) {
+
+        int cuadro = 6;
+        int n = 0;
+
+    while (n < cuadro){
+        for (int i = 0; i <cuadro; i++){
+            System.out.println("***************");
+            n += 1;
+        }
+    }
+    }
+
+}
