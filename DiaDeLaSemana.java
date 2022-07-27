c2196022784cd43edae4426fbc6b767498901892
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-27 00:06:55 -0500

    Add files via upload

---
+++ DiaDeLaSemana.java
@@ -0,0 +1,9 @@
+public enum DiaDeLaSemana {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO;
+}
---
+++ Fecha.java
@@ -0,0 +1,22 @@
+public class Fecha {
+    int dia;
+    int mes;
+    int año;
+    DiaDeLaSemana diaDeLaSemana;
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+    
+    public void setDiaDeLaSemana(DiaDeLaSemana diaDeLaSemana) {
+        this.diaDeLaSemana = diaDeLaSemana;
+    }
+}
