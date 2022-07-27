71fa36b0cee5abc9a78b431aae8ab3c34de83d24
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-27 16:20:56 -0500

    Create Prueba.py

---
+++ Prueba.py
@@ -0,0 +1,14 @@
+#
+#@author Daniel Romero
+#
+print ("Prueba python")
+a = 2
+b = 3
+c = 4
+d = a**a + ((b*2)-(c))
+##valor 6
+d+=2 ##valor 6 + 2
+if d % 2 == 0:
+   print("El número es par")
+else:
+    print("El número es impar")
