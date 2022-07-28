4b727880486f4115dc912394ebd4f504cfaa7e5f
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-27 22:55:59 -0500

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,14 @@
+package GarbageCollection;
+
+public class Persona {
+
+    private String nombre;
+
+    public String getNombre() { //Getter
+        return nombre;
+    }
+
+    public void setNombre(String nombre) { //Setter
+        this.nombre = nombre;
+    }
+}
