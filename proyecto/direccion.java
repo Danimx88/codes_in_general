73361caffa6b50ba99428d327ea1dbe7214ee317
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-28 23:42:23 -0500

    Create direccion.java

---
+++ proyecto/direccion.java
@@ -0,0 +1,55 @@
+public class Direccion {
+    private String calle;
+    private int numero;
+    private Ciudad ciudad;
+    private int codigoPostal;
+
+    public Direccion(String calle, int numero, Ciudad ciudad, int codigoPostal) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "calle='" + calle + '\'' +
+                ", numero='" + numero + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                ", codigoPostal=" + codigoPostal +
+                '}';
+    }
+}
