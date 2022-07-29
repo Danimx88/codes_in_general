b7fc7839d98d691f8eacae5d348610a659256d66
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-28 23:40:08 -0500

    Create Rect.java

---
+++ proyecto/Rect.java
@@ -0,0 +1,26 @@
+public class Rect {
+    private double alto;
+    private double ancho;
+
+    public Rect(double alto, double ancho) {
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+}
